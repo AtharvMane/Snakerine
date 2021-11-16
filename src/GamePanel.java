ae92ebfb5fb72a835d4564c39df4a25a07a3aa5b
AtharvMane <77918487+AtharvMane@users.noreply.github.com>
2021-11-16 14:31:21 +0530

    Modified the code to make multiplayer compatible

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,109 +2,148 @@ import javax.swing.*;
 import java.util.Random;
 import java.awt.*;
 import java.awt.event.*;
-public class GamePanel extends JPanel implements ActionListener{
-    public static final int MAX_HEIGHT=800, MAX_WIDTH=800,TILE_SIZE=25,DELAY=100;
-    private static int tileNumber= MAX_HEIGHT*MAX_WIDTH/TILE_SIZE;
+
+public class GamePanel extends JPanel implements ActionListener {
+    public static final int MAX_HEIGHT = 800, MAX_WIDTH = 800, TILE_SIZE = 25, DELAY = 100;
+    private static int tileNumber = MAX_HEIGHT * MAX_WIDTH / TILE_SIZE;
     private Timer timer;
     Random random;
-    protected char direction='R';
-    private boolean running=false;
-    Snakes[] snakes= new Snakes[2];
-    private int fruitPosX,fruitPosY;
+    protected char direction = 'R';
+    private boolean running = false;
+    Snakes[] snakes = new Snakes[2];
+    private int fruitPosX, fruitPosY;
     public int playerNumber;
-    GamePanel(){
-        this.setPreferredSize(new Dimension(MAX_WIDTH,MAX_HEIGHT));;
+
+    GamePanel() {
+        this.setPreferredSize(new Dimension(MAX_WIDTH, MAX_HEIGHT));
+        ;
         this.setBackground(Color.BLACK);
         this.setFocusable(true);
         this.addKeyListener(new myKeyAdapter());
-        random=new Random();
-        startGame(1);
-        
+        random = new Random();
+        startGame(2);
+
     }
+
     public void startGame(int i) {
-        this.running=true;
+        this.running = true;
         spawnFruit();
-        if(i==1){
-            playerNumber=1;
-            timer=new Timer(DELAY, this);
+        if (i == 1) {
+            playerNumber = 1;
+            timer = new Timer(DELAY, this);
             timer.start();
-            snakes[0]= new Snakes();
-        }
-        else if(i>1){
-            for (int j=0;j<i;j++){
-                snakes[j]=new Snakes();
+            snakes[0] = new Snakes(0, 0);
+        } else if (i > 1) {
+            for (int j = 0; j < i; j++) {
+                snakes[j] = new Snakes(random.nextInt((int) (MAX_HEIGHT / TILE_SIZE)) * TILE_SIZE,
+                        random.nextInt((int) (MAX_WIDTH / TILE_SIZE)) * TILE_SIZE);
             }
-            timer=new Timer(DELAY, this);
+            playerNumber = i;
+            timer = new Timer(DELAY, this);
             timer.start();
         }
     }
-    public void move(){
-        for (int j=0;j<playerNumber;j++){
+
+    public void move() {
+        for (int j = 0; j < playerNumber; j++) {
             snakes[j].move();
         }
     }
-    public void spawnFruit(){
-        fruitPosY=random.nextInt((int)(MAX_HEIGHT/TILE_SIZE))*TILE_SIZE;
-        fruitPosX=random.nextInt((int)(MAX_WIDTH/TILE_SIZE))*TILE_SIZE;
+
+    public void spawnFruit() {
+        fruitPosY = random.nextInt((int) (MAX_HEIGHT / TILE_SIZE)) * TILE_SIZE;
+        fruitPosX = random.nextInt((int) (MAX_WIDTH / TILE_SIZE)) * TILE_SIZE;
 
     }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         draw(g);
     }
-    public void draw(Graphics g) {
-        for (int i=0;i<(MAX_WIDTH/TILE_SIZE);i++){
-            g.drawLine(i*TILE_SIZE, 0, i*TILE_SIZE, MAX_HEIGHT);
-        }
-        for (int i=0;i<(MAX_HEIGHT/TILE_SIZE);i++){
-            g.drawLine(0,i*TILE_SIZE, MAX_HEIGHT,  i*TILE_SIZE);
-        }
-        
-        for (int i=0;i<snakes[0].length();i++){
-            if(i==0){
-            g.setColor(Color.red);
-            g.fillRect(snakes[0].xPos[i], snakes[0].yPos[i], TILE_SIZE, TILE_SIZE);
-            }
-            else{g.setColor(Color.green);
-            g.fillRect(snakes[0].xPos[i], snakes[0].yPos[i], TILE_SIZE, TILE_SIZE);}
 
-            
+    public void draw(Graphics g) {
+        for (int i = 0; i < (MAX_WIDTH / TILE_SIZE); i++) {
+            g.drawLine(i * TILE_SIZE, 0, i * TILE_SIZE, MAX_HEIGHT);
+        }
+        for (int i = 0; i < (MAX_HEIGHT / TILE_SIZE); i++) {
+            g.drawLine(0, i * TILE_SIZE, MAX_HEIGHT, i * TILE_SIZE);
+        }
+
+        for (int j = 0; j < playerNumber; j++) {
+            for (int i = 0; i < snakes[j].length(); i++) {
+                if (i == 0) {
+                    g.setColor(Color.red);
+                    g.fillRect(snakes[j].xPos[i], snakes[j].yPos[i], TILE_SIZE, TILE_SIZE);
+                } else {
+                    g.setColor(Color.green);
+                    g.fillRect(snakes[j].xPos[i], snakes[j].yPos[i], TILE_SIZE, TILE_SIZE);
+                }
+
+            }
         }
         g.setColor(Color.blue);
-        g.fillOval(fruitPosX, fruitPosY, TILE_SIZE,TILE_SIZE);
+        g.fillOval(fruitPosX, fruitPosY, TILE_SIZE, TILE_SIZE);
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         this.move();
-        snakes[0].checkSelfEat();
+        for (int i = 0; i < playerNumber; i++) {
+            for (int j = 0; j < playerNumber; j++) {
+                snakes[i].checkEat(snakes[j]);
+            }
+        }
 
-        if(snakes[0].checkFood(fruitPosX,fruitPosY)){
-            this.spawnFruit();
-        };        
+        for (int i = 0; i < playerNumber; i++) {
+            if (snakes[i].checkFood(fruitPosX, fruitPosY)) {
+                this.spawnFruit();
+            }
+
+        }
         repaint();
     }
-    public class myKeyAdapter extends KeyAdapter{
-        
+
+    public class myKeyAdapter extends KeyAdapter {
+
         @Override
         public void keyPressed(KeyEvent e) {
 
             switch (e.getKeyCode()) {
-                case KeyEvent.VK_LEFT:
-                    snakes[0].setDir('L');
-                    break;
-                case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_LEFT:
+                snakes[0].setDir('L');
+                break;
+            case KeyEvent.VK_RIGHT:
                 snakes[0].setDir('R');
+                break;
+            case KeyEvent.VK_UP:
+                snakes[0].setDir('U');
+                break;
+            case KeyEvent.VK_DOWN:
+                snakes[0].setDir('D');
+                break;
+
+            default:
+                break;
+            }
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_A:
+                    snakes[1].setDir('L');
                     break;
-                case KeyEvent.VK_UP:
-                snakes[0].setDir('U');   
+                case KeyEvent.VK_D:
+                    snakes[1].setDir('R');
                     break;
-                case KeyEvent.VK_DOWN:
-                snakes[0].setDir('D');      
+                case KeyEvent.VK_W:
+                    snakes[1].setDir('U');
                     break;
-            
+                case KeyEvent.VK_S:
+                    snakes[1].setDir('D');
+                    break;
+    
                 default:
                     break;
-            }
+                }
+            
+
         }
     }
 
--- src/Snakes.java
+++ src/Snakes.java
@@ -1,79 +1,90 @@
 import java.awt.event.*;
 import java.util.Random;
 import java.awt.*;
-public class Snakes {
-    private int length=1;
-    public int[] xPos=new int[40];
-    public int[] yPos=new int[40];
-    Random random=new Random();
-    private Character direction='R';
-    Snakes(){
-        xPos[0]=0;
-        yPos[0]=0;
-    }
-    public void move() {
-        for (int i=length;i>0;i--){
-            xPos[i]=xPos[i-1];
-            yPos[i]=yPos[i-1];
-            if(xPos[0]>GamePanel.MAX_WIDTH){
-                xPos[0]=0;
-            }
-            if(xPos[0]<0){
-                xPos[0]=(GamePanel.MAX_WIDTH);
 
-            }
-            if(yPos[0]>GamePanel.MAX_HEIGHT){
-                yPos[0]=0;
-            }
-            if(yPos[0]<0){
-                yPos[0]=(GamePanel.MAX_HEIGHT);
+public class Snakes {
+    private int length = 1;
+    public int[] xPos = new int[40];
+    public int[] yPos = new int[40];
+    Random random = new Random();
+    private Character direction = 'R';
+
+    Snakes(int a, int b) {
+        xPos[0] = a;
+        yPos[0] = b;
+    }
+
+    public void move() {
+        for (int i = length; i > 0; i--) {
+            xPos[i] = xPos[i - 1];
+            yPos[i] = yPos[i - 1];
+            if (xPos[0] > GamePanel.MAX_WIDTH-25) {
+                xPos[0] = 0;
                 
             }
-            
+            if (xPos[0] < 0) {
+                xPos[0] = (GamePanel.MAX_WIDTH-25);
+
+            }
+            if (yPos[0] > GamePanel.MAX_HEIGHT-25) {
+                yPos[0] = 0;
+            }
+            if (yPos[0] < 0) {
+                yPos[0] = (GamePanel.MAX_HEIGHT-25);
+
+            }
+            System.out.println(xPos[0]);
+
         }
         switch (this.direction) {
-            case 'R':
-                xPos[0]+=GamePanel.TILE_SIZE;
-                break;
-            case 'L':
-                xPos[0]-=GamePanel.TILE_SIZE;
-                break;
-            case 'U':
-                yPos[0]-=GamePanel.TILE_SIZE;
-                break;
-            case 'D':
-            yPos[0]+=GamePanel.TILE_SIZE;
-                break;
-            default:
-                break;
+        case 'R':
+            xPos[0] += GamePanel.TILE_SIZE;
+            break;
+        case 'L':
+            xPos[0] -= GamePanel.TILE_SIZE;
+            break;
+        case 'U':
+            yPos[0] -= GamePanel.TILE_SIZE;
+            break;
+        case 'D':
+            yPos[0] += GamePanel.TILE_SIZE;
+            break;
+        default:
+            break;
         }
     }
-    public void checkSelfEat() {
-        for(int i=1;i<this.length;i++){
-            if(xPos[0]==xPos[i] && yPos[0]==yPos[i]){
+
+    public void checkEat(Snakes snake) {
+        for (int i = 1; i < this.length; i++) {
+            if (snake.xPos[0] == xPos[i] && snake.yPos[0] == yPos[i]) {
                 System.out.println("collided self");
                 this.reduceLength(i);
             }
         }
     }
+
     private void reduceLength(int i) {
-        this.length=i;
+        this.length = i;
     }
-    
-    public boolean checkFood(int a, int b){
-        if(xPos[0]==a && yPos[0]==b){
+
+    public boolean checkFood(int a, int b) {
+        if (xPos[0] == a && yPos[0] == b) {
             this.length++;
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-    public int length(){
+
+    public int length() {
         return length;
     }
-    public void setDir(char a){
-        if(!((a=='L'&&this.direction=='R')||(a=='R'&&this.direction=='L')||(a=='U'&&this.direction=='D')||(a=='D'&&this.direction=='U'))){direction=a;}
+
+    public void setDir(char a) {
+        if (!((a == 'L' && this.direction == 'R') || (a == 'R' && this.direction == 'L')
+                || (a == 'U' && this.direction == 'D') || (a == 'D' && this.direction == 'U'))) {
+            direction = a;
+        }
     }
-    
 
 }
